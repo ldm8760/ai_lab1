685797c807bdf36cd78d4c81ffc897cabef8816b
logan <ldm8760@rit.edu>
2025-01-28 16:45:23 -0500

    hardcoded values

--- lab1.py
+++ lab1.py
@@ -1,7 +1,27 @@
 import sys
+from PIL import Image
+
+open_land = "#F89412"  # (248, 148, 18)
+rough_meadow = "#FFC000"  # (255, 192, 0)
+easy_movement_forest = "#FFFFFF"  # (255, 255, 255)
+slow_run_forest = "#02D03C"  # (2, 208, 60)
+walk_forest = "#028828"  # (2, 136, 40)
+impassible_vegetation = "#054918"  # (5, 73, 24)
+lake_swamp_marsh = "#0000FF"  # (0, 0, 255)
+paved_road = "#473303"  # (71, 51, 3)
+footpath = "#000000"  # (0, 0, 0)
+out_of_bounds = "#CD0065"  # (205, 0, 101)
+
+
+terrain = sys.argv[1]
+elevation = sys.argv[2]
+points_to_visit = sys.argv[3]
+output_file = sys.argv[4]
+
 
 def main():
-    print("Hello World!")
+    for i in sys.argv:
+        print(i)
 
 if __name__ == "__main__":
     if len(sys.argv) != 5:
