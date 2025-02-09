4e9524318f992679df899fc5b6e0ed3eddafb253
logan <ldm8760@rit.edu>
2025-02-08 22:01:39 -0500

    everything seems to be operational, going to gradescope

--- lab1.py
+++ lab1.py
@@ -3,23 +3,6 @@ import sys
 from PIL import Image, ImageColor
 import heapq
 
-def test_image():
-    im = Image.open("terrain.png")
-    pixels = im.load()
-    for x, y in coords:
-        pixels[x, y] = (0, 0, 0)  
-        pixels[x+1, y] = (0, 0, 0)  
-        pixels[x-1, y] = (0, 0, 0)  
-        pixels[x, y+1] = (0, 0, 0)  
-        pixels[x, y-1] = (0, 0, 0)  
-        pixels[x+1, y+1] = (0, 0, 0)  
-        pixels[x-1, y-1] = (0, 0, 0)  
-        pixels[x+1, y-1] = (0, 0, 0)  
-        pixels[x-1, y+1] = (0, 0, 0)
-
-    im.save("modified.png")
-    im.show()
-
 pixel_long = 10.29 # meters, east/west
 pixel_lat = 7.55 # meters, north/south
 pixel_area = pixel_long * pixel_lat
@@ -32,7 +15,6 @@ def calculate_traversal_score(color: str) -> int: # less is more
         score = float("inf")
     return round(score, 2)
 
-# format of hard coded colors is in {color: estimated meters per second}
 land_types: dict[str, float] = {
     "#F89412": 3,    # open_land
     "#FFC000": 2.2,  # rough_meadow
@@ -59,17 +41,6 @@ traversal_scores: dict[str, float] = {
     "#CD0065": calculate_traversal_score("#CD0065")   # out_of_bounds
 }
 
-# terrain = sys.argv[1]
-# elevation = sys.argv[2]
-# coords_file = sys.argv[3]
-# points_to_visit = []
-# with open(f"{coords_file}", "r") as f:
-#     for line in f:
-#         x, y = map(int, line.split())
-#         points_to_visit.append((x, y))  
-
-# output_file = sys.argv[4]
-
 coords = [
     (230, 327), (276, 279), (303, 240), (322, 242), (306, 286), 
     (319, 320), (325, 339), (312, 366), (275, 353), (253, 372), 
@@ -83,93 +54,86 @@ king_moves = [
     (1, -1), (1, 0), (1, 1)
 ]
 
-pos_x = 230
-pos_y = 327
-visited = [(pos_x, pos_y)]
-optimal_pixel = 25
-
 def chebyshev_distance(current_pos: tuple[int, int], next_goal: tuple[int, int]):
-    distance = max(abs(current_pos[0] - next_goal[0]), abs(current_pos[1] - next_goal[1]))
-    return distance
+    return max(abs(current_pos[0] - next_goal[0]), abs(current_pos[1] - next_goal[1]))
 
-def fn_score(start, current, goal, pixels) -> float:
-    hn = heuristic(current, goal)
-    gn = dist_to_start(current, start, pixels)
-    return round(hn + gn, 2)
+def position_cost(pixels, pos: tuple[int, int]) -> int:
+    rgb = pixels[pos[0], pos[1]]
+    cost = traversal_scores.get(f"#{rgb[0]:02x}{rgb[1]:02x}{rgb[2]:02x}".upper())
+    return cost
 
-def pixel_calc(pixel: tuple[int, int]) -> int:
-    t = traversal_scores.get(f"#{pixel[0]:02x}{pixel[1]:02x}{pixel[2]:02x}".upper())
-    return t
-
-def heuristic(current: tuple[int, int], goal: tuple[int, int]):
+def heuristic(current: tuple[int, int], goal: tuple[int, int]) -> float:
+    optimal_pixel = 25.9
     return chebyshev_distance(current, goal) * optimal_pixel
 
-# still needs to account for height but for now this works
-def dist_to_start(current: tuple[int, int], start: tuple[int, int], pixel):
-    dist = chebyshev_distance(current, start)
-    color = pixel_calc(pixel[current[0], current[1]])
-    try:
-        return dist * color
-    except Exception:
-        return dist * 26
+def reconstruct_path(came_from: dict, current: tuple[int, int]) -> list:
+    path = [current]
+    while current in came_from:
+        current = came_from[current]
+        path.append(current)
+    path.reverse()
+    return path
 
-def a_star_test(start: tuple[int, int], goal: tuple[int, int], pixels):
-    current = start
+def a_star_search(start: tuple[int, int], goal: tuple[int, int], pixels) -> list:
     pq = []
-    heapq.heappush(pq, (fn_score(start, current, goal, pixels), current))
-    vis = set()
+    heapq.heappush(pq, (heuristic(start, goal), start))
+
+    g_score: dict[tuple[int, int], int] = {start: 0}
+    came_from: dict[tuple[int, int], tuple[int, int]] = {}
 
     while pq:
-        priority, current = heapq.heappop(pq)
-        if current in vis:
-            continue
-
-        print(f"priority_score: {priority}, current pixel: {current}")
-
-
-        if current == goal:
-            visited.append(coords[len(visited)])
-            break
-
-        # check: dict[tuple[int, int], float] = {}
-        for dx, dy in king_moves:
-            neighbor_x = current[0] + dx
-            neighbor_y = current[1] + dy
-            neighbor = (neighbor_x, neighbor_y)
-            if neighbor in vis:
-                # print("yep its here")
-                # print(fn_score(start, neighbor, goal, pixels))
-                continue
-
-            heapq.heappush(pq, (fn_score(start, neighbor, goal, pixels), neighbor))
-            # check[neighbor] = fn_score(start, neighbor, goal, pixels)
-
+        _, current = heapq.heappop(pq)
         
-        best_neighbor = heapq.heappop(pq)
-        # print(f"best neighbor: {best_neighbor}")
-        # pq.put(best_neighbor)
-        vis.add(current)
-        # path.append(best_neighbor)
-        # pixels[best_neighbor[0], best_neighbor[1]] = (ImageColor.getcolor("#a146dd", "RGB"))
+        if current == goal:
+            return reconstruct_path(came_from, current)
 
+        for dx, dy in king_moves:
+            neighbor = (current[0] + dx, current[1] + dy)
+
+            tentative_g = g_score[current] + position_cost(pixels, neighbor)
+
+            if neighbor not in g_score or tentative_g < g_score[neighbor]:
+                came_from[neighbor] = current
+                g_score[neighbor] = tentative_g
+                f_score = tentative_g + heuristic(neighbor, goal)
+                heapq.heappush(pq, (f_score, neighbor))
+
+    return []
+
+visited = [(230, 327)]
+terrain = sys.argv[1]
+elevation = sys.argv[2]
+coords_file = sys.argv[3]
+output_file = sys.argv[4]
+points_to_visit = []
+with open(f"{coords_file}", "r") as f:
+    for line in f:
+        x, y = map(int, line.split())
+        points_to_visit.append((x, y))  
 
 def main():
-    # im = Image.open(f"{terrain}")
-    im = Image.open("terrain.png")
+    im = Image.open(f"{terrain}")
+    # im = Image.open("terrain.png")
     im = im.convert("RGB")
     pixels = im.load()
 
-    # print(coords[0], coords[1])
-    # for i in range(len(coords)):
-    #     a_star_test(coords[i], coords[i + 1], pixels)
-    a_star_test(coords[0], (250, 347), pixels)
+    total_path = []
+    for i in range(len(points_to_visit) - 1):
+        path = a_star_search(points_to_visit[i], points_to_visit[i + 1], pixels)
 
-    # im.save(f"{output_file}")
-    im.save(f"modified.png")
+        for position in path:
+            total_path.append(position)         
+
+    print(len(total_path) * pixel_area)
+
+    for x, y in total_path:
+        pixels[x, y] = ImageColor.getcolor("#a146dd", "RGB")
+
+    im.save(f"{output_file}")
+    # im.save(f"modified.png")
     im.show()
 
 if __name__ == "__main__":
-    # if len(sys.argv) != 5:
-    #     raise ValueError(f"Four arguments required, got {len(sys.argv) - 1}")
-    
+    if len(sys.argv) != 5:
+        raise ValueError(f"Four arguments required, got {len(sys.argv) - 1}")
     main()
