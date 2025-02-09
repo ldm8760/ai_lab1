92a6d430729b8b857a77ef4ca209f9b869c49b6d
ldm8760 <ldm8760@rit.edu>
2025-02-08 21:05:09 -0500

    stuff from office hours

--- lab1.py
+++ lab1.py
@@ -1,7 +1,7 @@
 from random import Random
 import sys
-from PIL import Image
-from queue import PriorityQueue
+from PIL import Image, ImageColor
+import heapq
 
 def test_image():
     im = Image.open("terrain.png")
@@ -59,10 +59,16 @@ traversal_scores: dict[str, float] = {
     "#CD0065": calculate_traversal_score("#CD0065")   # out_of_bounds
 }
 
-terrain = sys.argv[1]
-elevation = sys.argv[2]
-points_to_visit = sys.argv[3]
-output_file = sys.argv[4]
+# terrain = sys.argv[1]
+# elevation = sys.argv[2]
+# coords_file = sys.argv[3]
+# points_to_visit = []
+# with open(f"{coords_file}", "r") as f:
+#     for line in f:
+#         x, y = map(int, line.split())
+#         points_to_visit.append((x, y))  
+
+# output_file = sys.argv[4]
 
 coords = [
     (230, 327), (276, 279), (303, 240), (322, 242), (306, 286), 
@@ -80,7 +86,7 @@ king_moves = [
 pos_x = 230
 pos_y = 327
 visited = [(pos_x, pos_y)]
-optimal_pixel = 25.9
+optimal_pixel = 25
 
 def chebyshev_distance(current_pos: tuple[int, int], next_goal: tuple[int, int]):
     distance = max(abs(current_pos[0] - next_goal[0]), abs(current_pos[1] - next_goal[1]))
@@ -89,9 +95,7 @@ def chebyshev_distance(current_pos: tuple[int, int], next_goal: tuple[int, int])
 def fn_score(start, current, goal, pixels) -> float:
     hn = heuristic(current, goal)
     gn = dist_to_start(current, start, pixels)
-    return hn + gn
-
-
+    return round(hn + gn, 2)
 
 def pixel_calc(pixel: tuple[int, int]) -> int:
     t = traversal_scores.get(f"#{pixel[0]:02x}{pixel[1]:02x}{pixel[2]:02x}".upper())
@@ -102,61 +106,70 @@ def heuristic(current: tuple[int, int], goal: tuple[int, int]):
 
 # still needs to account for height but for now this works
 def dist_to_start(current: tuple[int, int], start: tuple[int, int], pixel):
-    return chebyshev_distance(current, start) * pixel_calc(pixel[current[0], current[1]])
-
-
+    dist = chebyshev_distance(current, start)
+    color = pixel_calc(pixel[current[0], current[1]])
+    try:
+        return dist * color
+    except Exception:
+        return dist * 26
 
 def a_star_test(start: tuple[int, int], goal: tuple[int, int], pixels):
     current = start
-    pq = PriorityQueue(100)
-    pq.put(current)
-    vis = set(current)
+    pq = []
+    heapq.heappush(pq, (fn_score(start, current, goal, pixels), current))
+    vis = set()
 
-    print(fn_score(start, current, goal, pixels))
+    while pq:
+        priority, current = heapq.heappop(pq)
+        if current in vis:
+            continue
 
-    while pq.not_empty:
-        current = pq.get()
-        check: dict[tuple[int, int], float] = {}
-        for dx, dy in king_moves:
-            neighbor_x = current[0] + dx
-            neighbor_y = current[1] + dy
-            new = (neighbor_x, neighbor_y)
-            if new in vis:
-                continue
+        print(f"priority_score: {priority}, current pixel: {current}")
 
-            check[new] = fn_score(start, current, goal, pixels)
-            # gn = gn_score((neighbor_x, neighbor_y), start, pixels)
-            # print(f"pos_x: {neighbor_x}, pos_y: {neighbor_y}")
-            # print(f"g(n): {gn}")
-            # hn = heuristic((neighbor_x, neighbor_y), goal)
-            # print(f"h(n): {hn}")
-            # fn = fn_score(gn, hn)
-            # print(f"f(n): {fn}")
-            # print("-----")
-
-        
-        # print(f"f(n): {check, check[new]}")
-        print(min(check, key=check.get))
 
         if current == goal:
             visited.append(coords[len(visited)])
             break
-    
+
+        # check: dict[tuple[int, int], float] = {}
+        for dx, dy in king_moves:
+            neighbor_x = current[0] + dx
+            neighbor_y = current[1] + dy
+            neighbor = (neighbor_x, neighbor_y)
+            if neighbor in vis:
+                # print("yep its here")
+                # print(fn_score(start, neighbor, goal, pixels))
+                continue
+
+            heapq.heappush(pq, (fn_score(start, neighbor, goal, pixels), neighbor))
+            # check[neighbor] = fn_score(start, neighbor, goal, pixels)
+
+        
+        best_neighbor = heapq.heappop(pq)
+        # print(f"best neighbor: {best_neighbor}")
+        # pq.put(best_neighbor)
+        vis.add(current)
+        # path.append(best_neighbor)
+        # pixels[best_neighbor[0], best_neighbor[1]] = (ImageColor.getcolor("#a146dd", "RGB"))
 
 
 def main():
+    # im = Image.open(f"{terrain}")
     im = Image.open("terrain.png")
     im = im.convert("RGB")
     pixels = im.load()
 
     # print(coords[0], coords[1])
-    a_star_test(coords[0], coords[1], pixels)
+    # for i in range(len(coords)):
+    #     a_star_test(coords[i], coords[i + 1], pixels)
+    a_star_test(coords[0], (250, 347), pixels)
 
-    im.save("terrain.png")
-    # im.show()   
+    # im.save(f"{output_file}")
+    im.save(f"modified.png")
+    im.show()
 
 if __name__ == "__main__":
-    if len(sys.argv) != 5:
-        raise ValueError(f"Four arguments required, got {len(sys.argv) - 1}")
+    # if len(sys.argv) != 5:
+    #     raise ValueError(f"Four arguments required, got {len(sys.argv) - 1}")
     
     main()
